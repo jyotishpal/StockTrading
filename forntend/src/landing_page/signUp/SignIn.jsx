4a6c07720ed646c95d8136b9c22fcd3dcd45523f
jyotish kumar pal <paljyotish36@gmail.com>
2025-10-04 00:12:51 +0530

    Update SignIn.jsx

--- forntend/src/landing_page/signUp/SignIn.jsx
+++ forntend/src/landing_page/signUp/SignIn.jsx
@@ -18,7 +18,7 @@ function SignIn() {
     if(res.data.success){
         // alert("Login successfull!")
          // full redirect to dashboard project
-       window.open("https://stocktrading-forntend.onrender.com", "_blank");
+       window.open("https://stocktrading-dashboard.onrender.com/", "_blank");
  // dashboard URL
     }else{
       alert(res.data.message||"Invalid Credentials")
