df7f4ac4deb20aaede46443ddfb98d063287cc03
jyotish kumar pal <paljyotish36@gmail.com>
2025-10-04 01:10:07 +0530

    Update main.jsx

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter,Routes,Route } from 'react-router-dom'
 import Home from './components/Home';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
+    <HashRouter>
   <Routes>
     <Route path="*" element={<Home/>}></Route>
   </Routes>
-  </BrowserRouter>
+   </HashRouter>
   </StrictMode>,
 )
